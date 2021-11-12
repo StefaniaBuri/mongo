1c28425acd7f32c58b3d833c9f9b5cfef21907d4
StefaniaBuri <stefania_0506@hotmail.com>
2021-11-12 22:11:27 +0100

    dia1

---
+++ dia1/mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+let User = require("./schema");
+let Profile = require("./schema");
+let Credentials = require("./schema");
+
+mongoose.connect("mongodb://localhost:27017/mongo",
+                {useNewUrlParser: false, useUnifiedTopology: false }) 
+                
+/*
+let userDocument = new User({
+    login: "nia",
+    password: "0506",
+});
+
+userDocument.save(checkRespuesta)
+
+function checkRespuesta(err,res){
+    if(err)
+     console.log("Error: " + err)
+    else{
+     console.log("Documento guardado correctamente")
+     console.log(res)
+    }
+}
+
+
+let profileDocument = new Profile({
+    name:"Andre",
+    surname: "Custodio",
+    dateOfBirth:"08/07/1991",
+    comments:"Me gusta tocar la guitarra",
+    role:"admin"
+});
+
+profileDocument.save(checkRespuesta)
+
+function checkRespuesta(err,res){
+    if(err)
+     console.log("Error: " + err)
+    else{
+     console.log("Documento guardado correctamente")
+     console.log(res)
+    }
+}
+*/
+
+let credentialsDocument = new Credentials({
+    address:"c/Boltaña 3",
+    phone: "074732789",
+    email:"andre@gmail.com"
+});
+
+credentialsDocument.save(checkRespuesta)
+
+function checkRespuesta(err,res){
+    if(err)
+     console.log("Error: " + err)
+    else{
+     console.log("Documento guardado correctamente")
+     console.log(res)
+    }
+}
---
+++ dia1/schema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose"); 
+
+/*
+const UserSchema = new mongoose.Schema({
+    login: String,
+    password: String
+})
+
+const ProfileSchema = new mongoose.Schema({
+    name: String,
+    surname: String,
+    dateOfBirth: String,
+    comments: String,
+    role: String
+})
+*/
+const CredentialsSchema= new mongoose.Schema({
+    address: String,
+    phone: String,
+    email: String
+})
+
+// module.exports = mongoose.model("User", UserSchema)
+// module.exports = mongoose.model("Profile", ProfileSchema)
+module.exports = mongoose.model("Credentials", CredentialsSchema)
